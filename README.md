5ddd0548f429d271578b3b6392702d40c1f639d4
bbnoir <bbnoir83@gmail.com>
2023-10-17 22:13:01 +0800

    add detect method

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ai-server
+
+Run the server locally with:
+```
+FLASK_APP=app.py flask run
+```
---
+++ detect.py
@@ -0,0 +1,80 @@
+import cv2
+import torch
+from numpy import random
+
+from models.experimental import attempt_load
+from utils.datasets import LoadImages
+from utils.general import check_img_size, non_max_suppression, scale_coords, xyxy2xywh
+from utils.plots import plot_one_box
+
+
+def detect(source='buffer/image.jpg'):
+    weights = 'model.pt'
+    imgsz = 640
+    conf_thres = 0.25
+    iou_thres = 0.45
+
+    # Initialize
+    device = torch.device('cpu')
+
+    # Load model
+    model = attempt_load(weights, map_location=device)  # load FP32 model
+    stride = int(model.stride.max())  # model stride
+    imgsz = check_img_size(imgsz, s=stride)  # check img_size
+
+    # Set Dataloader
+    dataset = LoadImages(source, img_size=imgsz, stride=stride)
+
+    # Get names and colors
+    names = model.module.names if hasattr(model, 'module') else model.names
+    colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
+
+    for path, img, im0s, vid_cap in dataset:
+        img = torch.from_numpy(img).to(device)
+        img = img.float()  # uint8 to fp16/32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        if img.ndimension() == 3:
+            img = img.unsqueeze(0)
+
+        # Inference
+        with torch.no_grad():   # Calculating gradients would cause a GPU memory leak
+            pred = model(img, augment=False)[0]
+
+        # Apply NMS
+        pred = non_max_suppression(pred, conf_thres, iou_thres, classes=None, agnostic=False)
+
+        # Process detections
+        for i, det in enumerate(pred):  # detections per image
+            p, s, im0, frame = path, '', im0s, getattr(dataset, 'frame', 0)
+
+            img_path = 'buffer/result.jpg'
+            txt_path = 'buffer/label.txt'
+            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+
+                for c in det[:, -1].unique():
+                    n = (det[:, -1] == c).sum()  # detections per class
+                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+                # Write results
+                for *xyxy, conf, cls in reversed(det):
+                    # Write to file
+                    xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                    line = (cls, *xywh, conf) # label format
+                    with open(txt_path, 'w') as f:
+                        f.write(('%g ' * len(line)).rstrip() % line + '\n')
+
+                    # Add bbox to image
+                    label = f'{names[int(cls)]} {conf:.2f}'
+                    plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=1)
+
+            # Save results (image with detections)
+            cv2.imwrite(img_path, im0)
+            
+            return img_path, txt_path
+
+if __name__ == '__main__':
+    with torch.no_grad():
+        detect()
