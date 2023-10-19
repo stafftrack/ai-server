4036957912eedbee4bfa80ff8f9b47304d967b06
bbnoir <bbnoir83@gmail.com>
2023-10-19 20:36:36 +0800

    mod response

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from utils.datasets import LoadImages
 from utils.general import check_img_size, non_max_suppression, scale_coords, xyxy2xywh
 from utils.plots import plot_one_box
 
-from flask import Flask, jsonify, request, send_file
+from flask import Flask, jsonify, request, send_file, Response
 
 app = Flask(__name__)
 
@@ -35,6 +35,8 @@ def detect(source='buffer/image.jpg'):
     names = model.module.names if hasattr(model, 'module') else model.names
     colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
 
+    danger = False
+    class_count = [0, 0, 0, 0, 0]
     for path, img, im0s, vid_cap in dataset:
         img = torch.from_numpy(img).to(device)
         img = img.float()  # uint8 to fp16/32
@@ -57,6 +59,7 @@ def detect(source='buffer/image.jpg'):
             txt_path = 'buffer/label.txt'
             gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
             if len(det):
+                danger = True
                 # Rescale boxes from img_size to im0 size
                 det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
 
@@ -75,19 +78,29 @@ def detect(source='buffer/image.jpg'):
                     # Add bbox to image
                     label = f'{names[int(cls)]} {conf:.2f}'
                     plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=1)
+                    class_count[int(cls)] += 1
 
             # Save results (image with detections)
             cv2.imwrite(img_path, im0)
             
-            return img_path, txt_path
+            return img_path, danger, class_count
 
 @app.route("/infer", methods=["POST"])
 def predict():
     if request.method == "POST":
         file = request.files["file"]
         file.save("buffer/image.jpg")
-        img_path, txt_path = detect("buffer/image.jpg")
-        return send_file(img_path)
+        img_path, danger, class_count = detect("buffer/image.jpg")
+        if danger:
+            return {
+                "electronic_device": class_count[0],
+                "laptop": class_count[1],
+                "scissors": class_count[2],
+                "knife": class_count[3],
+                "gun": class_count[4],
+            }
+        else:
+            return Response(status=200, mimetype='application/json', response='null')
 
 def run():
     app.debug = True
@@ -96,6 +109,4 @@ def run():
 
 
 if __name__ == "__main__":
-    app.debug = True
-    port = int(os.environ.get("PORT", 80))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    run()
