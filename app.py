060703c21e0395118d8847d1117f6d5437b54a95
bbnoir <bbnoir83@gmail.com>
2023-10-22 12:24:43 +0800

    port

--- app.py
+++ app.py
@@ -129,7 +129,7 @@ def predict():
 
 def run():
     app.debug = True
-    port = int(os.environ.get("PORT", 80))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host='0.0.0.0', port=port, debug=True)
 
 
